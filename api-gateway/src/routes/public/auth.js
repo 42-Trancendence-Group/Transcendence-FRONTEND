3908fca77698654eeadc64a5a5791990378a940b
Iva <104653667+pin3dev@users.noreply.github.com>
2025-04-20 20:04:10 -0300

    fix: automated proxy adaptation

--- api-gateway/src/routes/public/auth.js
+++ api-gateway/src/routes/public/auth.js
@@ -1,33 +1,33 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
-module.exports = async function (fastify) {
-  fastify.post("/auth/register", async (request, reply) => {
-    console.log("📥 [GATEWAY] Requisição recebida no /auth/register");
-    console.log("🔎 Corpo recebido:", request.body);
+// module.exports = async function (fastify) {
+//   fastify.post("/auth/register", async (request, reply) => {
+//     console.log("📥 [GATEWAY] Requisição recebida no /auth/register");
+//     console.log("🔎 Corpo recebido:", request.body);
 
-    try {
-      const res = await fetch("http://auth-service:4000/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(request.body),
-        timeout: 5000
-      });
+//     try {
+//       const res = await fetch("http://auth-service:4000/register", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(request.body),
+//         timeout: 5000
+//       });
 
-      const data = await res.json();
+//       const data = await res.json();
 
-      console.log("✅ [GATEWAY] Resposta do auth-service:", data);
+//       console.log("✅ [GATEWAY] Resposta do auth-service:", data);
 
-      return reply.code(res.status).send(data);
+//       return reply.code(res.status).send(data);
 
-    } catch (err) {
-      console.error("❌ [GATEWAY] Erro ao chamar auth-service:", err.name, err.message);
+//     } catch (err) {
+//       console.error("❌ [GATEWAY] Erro ao chamar auth-service:", err.name, err.message);
 
-      return reply.code(500).send({
-        error: "Erro interno no gateway",
-        detalhe: err.message
-      });
-    }
-  });
-};
+//       return reply.code(500).send({
+//         error: "Erro interno no gateway",
+//         detalhe: err.message
+//       });
+//     }
+//   });
+// };
